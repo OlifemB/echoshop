7d4a279ce3b72c0c0ab1d30af88b6a4f4c35b18d
Olifem <olifemb@gmail.com>
2025-07-22 22:44:35 +0300

    next pages ssr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,10 @@
-import { NextPage } from "next";
 import { AppProps } from "next/app";
-import React, { ReactElement, ReactNode } from "react"
 import { AntdRegistry } from '@ant-design/nextjs-registry'
 import { RootLayout } from "@/components/shared/Layout";
 import '@/common/styles/globals.css'
 
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
 
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
-
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <AntdRegistry>
       <RootLayout>
